7b38cf69608c0e9d564b7c3ecc86e32c3cd20a74
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-04-28 15:47:49 -0500

    added springboot framework

---
+++ src/test/java/com/codeup/foodtruckfinder/FoodTruckFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.foodtruckfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

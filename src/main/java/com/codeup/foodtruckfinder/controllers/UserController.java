410c06fd077fc7fcf38faae67772e1b3b3e9e2da
Isabella <isabellarose.hernandez01@gmail.com>
2022-05-02 16:39:20 -0500

    working on user controller and partials problems

--- src/main/java/com/codeup/foodtruckfinder/controllers/UserController.java
+++ src/main/java/com/codeup/foodtruckfinder/controllers/UserController.java
@@ -5,11 +5,12 @@ import com.codeup.foodtruckfinder.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-    private UserRepository userDao;
+    private final UserRepository userDao;
 
     public UserController(UserRepository userDao) {
         this.userDao = userDao;
@@ -22,8 +23,20 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String registerUser() {
+    public String registerUser(@ModelAttribute User user) {
+        userDao.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/about")
+    public String aboutUs() {
+        return "about";
+    }
+
+    @GetMapping("/editUser")
+    public String editUserForm() {
+
+        return "editUser";
     }
 
 }

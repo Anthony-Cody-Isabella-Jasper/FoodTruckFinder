ddcdfb6fa08e502dad70c5bc843ec18db7098a65
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-05-02 15:15:43 -0500

    finished truck controller and repository

--- src/main/java/com/codeup/foodtruckfinder/controllers/TruckController.java
+++ src/main/java/com/codeup/foodtruckfinder/controllers/TruckController.java
@@ -2,6 +2,7 @@ package com.codeup.foodtruckfinder.controllers;
 
 import com.codeup.foodtruckfinder.repositories.TruckRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TruckController {
@@ -10,4 +11,31 @@ public class TruckController {
     public TruckController(TruckRepository truckDao) {
         this.truckDao = truckDao;
     }
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping
+    public String editTruck(){
+        return "editTruck";
+    }
+
+    @GetMapping
+    public String showTruck(){
+        return "individual";
+    }
+
+    @GetMapping
+    public String truckProfile(){
+        return "profile";
+    }
+
+
+
+
+
+
+
 }

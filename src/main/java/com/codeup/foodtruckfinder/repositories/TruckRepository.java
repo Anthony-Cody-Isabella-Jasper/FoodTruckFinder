f25d4bc7b92d222ee446ed5f600ec749957de740
Isabella <isabellarose.hernandez01@gmail.com>
2022-05-03 09:49:01 -0500

    user controller in progress

--- src/main/java/com/codeup/foodtruckfinder/repositories/TruckRepository.java
+++ src/main/java/com/codeup/foodtruckfinder/repositories/TruckRepository.java
@@ -1,8 +1,12 @@
 package com.codeup.foodtruckfinder.repositories;
 
+import com.codeup.foodtruckfinder.models.TruckPicture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeup.foodtruckfinder.models.Truck;
 
+import java.util.List;
+
 public interface TruckRepository extends JpaRepository <Truck, Long> {
     Truck getTruckById(long id);
+    Truck getByTruckPictures();
 }

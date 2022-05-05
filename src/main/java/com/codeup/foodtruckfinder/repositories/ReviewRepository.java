06579613e27cfbed1c5decd3ec3d09d16c56a1bc
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-05-05 14:36:54 -0500

    added average ratings and clear filter button

--- src/main/java/com/codeup/foodtruckfinder/repositories/ReviewRepository.java
+++ src/main/java/com/codeup/foodtruckfinder/repositories/ReviewRepository.java
@@ -4,6 +4,7 @@ import com.codeup.foodtruckfinder.models.Review;
 import com.codeup.foodtruckfinder.models.Truck;
 import com.codeup.foodtruckfinder.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 

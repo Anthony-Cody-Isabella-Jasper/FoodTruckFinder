b68e9c93b83024657df482231632e88b374bb776
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-05-06 12:09:29 -0500

    added save truck location functionality

--- src/main/java/com/codeup/foodtruckfinder/controllers/TruckController.java
+++ src/main/java/com/codeup/foodtruckfinder/controllers/TruckController.java
@@ -69,14 +69,16 @@ public class TruckController {
         return "truck/individual";
     }
 
-//    @PostMapping("/truck/{id}/show")
-//    public String addToFav(@PathVariable Long id, @ModelAttribute Truck truck) {
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//
-//        truck.setUsersFavorited(user);
-//
-//        return "/profile";
-//    }
+    @PostMapping("/truck/{id}/located")
+    public String setTruckLocation(Model model, @PathVariable Long id, @RequestParam("longitude") double longitude, @RequestParam("latitude") double latitude) {
+        Truck truck = truckDao.getTruckById(id);
+        truck.setLatitude(latitude);
+        truck.setLongitude(longitude);
+        truckDao.save(truck);
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("truck", truck);
+        return "truck/individual";
+    }
 
     @GetMapping("/truck/{id}/profile")
     public String truckProfile(@PathVariable Long id, Model model) {

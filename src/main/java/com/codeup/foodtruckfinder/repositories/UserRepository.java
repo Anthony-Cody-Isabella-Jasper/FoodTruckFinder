fb2cb324fe75f1dfad1c6ec32890c024f7567f01
Cody Hodges <hodges.cody00@gmail.com>
2022-05-09 15:12:50 -0500

    Cody

--- src/main/java/com/codeup/foodtruckfinder/repositories/UserRepository.java
+++ src/main/java/com/codeup/foodtruckfinder/repositories/UserRepository.java
@@ -14,4 +14,11 @@ User findByUsername(String username);
     @Query(value = "DELETE FROM user_favorites u WHERE u.user_id = ?1 AND u.truck_id = ?2", nativeQuery = true)
     void deleteFavorite(long userId, Long truckId);
 
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM user_favorites u WHERE u.user_id = ?", nativeQuery = true)
+    void deleteUserFavorite(long userId);
+
+
+
 }

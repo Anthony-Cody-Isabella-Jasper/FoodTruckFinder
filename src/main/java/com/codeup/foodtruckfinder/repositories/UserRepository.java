fa5c29818cc9b859cdb5171971359f1713aa7940
Isabella <isabellarose.hernandez01@gmail.com>
2022-05-09 15:49:23 -0500

    update password functionality

--- src/main/java/com/codeup/foodtruckfinder/repositories/UserRepository.java
+++ src/main/java/com/codeup/foodtruckfinder/repositories/UserRepository.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 import javax.transaction.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-User findByUsername(String username);
+    User findByUsername(String username);
+
     @Transactional
     @Modifying
     @Query(value = "DELETE FROM user_favorites u WHERE u.user_id = ?1 AND u.truck_id = ?2", nativeQuery = true)
     void deleteFavorite(long userId, Long truckId);
-
 }

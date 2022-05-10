4b8935f7deb4c8aec2af9d66ea296bcc42297d80
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-05-10 14:22:59 -0500

    implemented reject button for user approval page

--- src/main/java/com/codeup/foodtruckfinder/controllers/UserController.java
+++ src/main/java/com/codeup/foodtruckfinder/controllers/UserController.java
@@ -39,11 +39,11 @@ public class UserController {
 
     @PostMapping("/register")
     public String registerUser(@ModelAttribute User user, Model model) {
-        if(userDao.existsUserByEmail(user.getEmail())) {
+        if (userDao.existsUserByEmail(user.getEmail())) {
             model.addAttribute("user", new User());
             model.addAttribute("message", "Email already exists. Please click on \"Forgot Password\" when logging in to retrieve your password.");
             return "/register";
-        } else if(userDao.existsUserByUsername(user.getUsername())) {
+        } else if (userDao.existsUserByUsername(user.getUsername())) {
             model.addAttribute("user", new User());
             model.addAttribute("message", "Username already exists. Please pick a different username.");
             return "/register";
@@ -98,11 +98,11 @@ public class UserController {
 
     @PostMapping("/editUser/{id}")
     public String editUser(Model model, @ModelAttribute User user, HttpSession session, @RequestParam("oldPass") String oldPass, @RequestParam("newPass") String newPass) {
-        if(userDao.existsUserByEmail(user.getEmail())) {
+        if (userDao.existsUserByEmail(user.getEmail())) {
             model.addAttribute("user", userDao.getById(user.getId()));
             model.addAttribute("message", "Email already exists. Please click on \"Forgot Password\" when logging in to retrieve your password.");
             return "/editUser";
-        } else if(userDao.existsUserByUsername(user.getUsername())) {
+        } else if (userDao.existsUserByUsername(user.getUsername())) {
             model.addAttribute("user", userDao.getById(user.getId()));
             model.addAttribute("message", "Username already exists. Please pick a different username.");
             return "/editUser";
@@ -150,7 +150,7 @@ public class UserController {
     public String forgotPasswordSubmission(Model model, User user) {
         String userEmail = user.getEmail();
 
-        if(userEmail != null) {
+        if (userEmail != null) {
         }
 
 
@@ -172,11 +172,11 @@ public class UserController {
 
     @PostMapping("/approve")
     public String approved(Model model, @RequestParam(name = "pendingId") Long pendingId, @RequestParam(name = "username") String username, @RequestParam(name = "email") String email, @RequestParam(name = "password") String password) {
-        if(userDao.existsUserByEmail(email)) {
+        if (userDao.existsUserByEmail(email)) {
             model.addAttribute("pendingUsers", pendingTruckDao.findAll());
             model.addAttribute("message", "Email already exists. Please click on \"Forgot Password\" when logging in to retrieve your password.");
             return "/approve";
-        } else if(userDao.existsUserByUsername(username)) {
+        } else if (userDao.existsUserByUsername(username)) {
             model.addAttribute("pendingUsers", pendingTruckDao.findAll());
             model.addAttribute("message", "Username already exists. Please pick a different username.");
             return "/approve";
@@ -188,6 +188,12 @@ public class UserController {
         newTruck.setTruck_owner(newUser);
         userDao.save(newUser);
         truckDao.save(newTruck);
+        pendingTruckDao.deleteById(pendingId);
+        return "redirect:/approve";
+    }
+
+    @PostMapping("/reject")
+    public String rejected(@RequestParam(name = "pendingId") Long pendingId) {
         pendingTruckDao.deleteById(pendingId);
         return "redirect:/approve";
     }

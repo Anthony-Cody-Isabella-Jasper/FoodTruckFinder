f02d187f6dc8dc0f513eb9f7038936636ed1ba47
Isabella <isabellarose.hernandez01@gmail.com>
2022-05-16 14:15:16 -0500

    test commit redeploy to prod to spin up jpas and see tables built -kh

--- migrator.sql
+++ migrator.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS food_truck_finder_db;
-CREATE DATABASE IF NOT EXISTS food_truck_finder_db;
+# DROP DATABASE IF EXISTS food_truck_finder_db;
+# CREATE DATABASE IF NOT EXISTS food_truck_finder_db;
 DROP USER IF EXISTS 'ftfuser'@'localhost';
 CREATE USER 'ftfuser'@'localhost' IDENTIFIED BY 'codeup';
 GRANT ALL ON food_truck_finder_db.* TO 'ftfuser'@'localhost';

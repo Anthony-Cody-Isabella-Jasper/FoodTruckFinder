6e385df145085747818875ba260d182dedaa72e2
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-05-20 09:33:58 -0500

    added easter egg

---
+++ src/main/resources/static/js/konami.js
@@ -0,0 +1,20 @@
+"use strict";
+let changeCursor = () => {
+	document.body.classList.add("konami");
+}
+let keypressArray = [];
+let code = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a'];
+let success = false;
+document.addEventListener("keyup", event => {
+	keypressArray.push(event.key.toLowerCase());
+	keypressArray.forEach((key, index) => {
+		if (key === code[index]) {
+			success = true;
+		} else {
+			success = false;
+		}
+	});
+	if (success && keypressArray.length === 10) {
+		changeCursor();
+	}
+});
